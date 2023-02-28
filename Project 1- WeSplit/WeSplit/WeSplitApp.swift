93d947bd3b6e083452593dfeb75487cefbacc438
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-02-28 19:52:54 +0530

    Start Project 1

---
+++ Project 1- WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,19 @@
+//
+//	WeSplitApp.swift
+//	WeSplit
+//
+//	Created by Prathamesh Kowarkar on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+
+}
