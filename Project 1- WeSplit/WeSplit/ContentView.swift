29019752c1b26cfa7731987ddc948ab3ca1563e8
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-02-28 19:57:46 +0530

    Create a form

--- Project 1- WeSplit/WeSplit/ContentView.swift
+++ Project 1- WeSplit/WeSplit/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
 
 	var body: some View {
-		VStack {
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundColor(.accentColor)
-			Text("Hello, world!")
+		Form {
+			Section {
+				Text("Hello, world!")
+			}
+
+			Section {
+				Text("Hello, world!")
+				Text("Hello, world!")
+			}
 		}
-		.padding()
 	}
 
 }
